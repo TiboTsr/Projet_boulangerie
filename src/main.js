c59e480d766d9a0ee688516fae77f93fa1d3f46a
TiboTsr <tibo.tessier@gmail.com>
2024-12-08 12:34:40 +0100

    Boulangerie_Update_1.2.2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-import './style.css'
+// Menu déroulant
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('nav ul');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('nav ul');
+  if (menuToggle && menu) {
+      // Gère le clic sur le bouton de menu
+      menuToggle.addEventListener('click', function () {
+          menu.classList.toggle('show');
+      });
+  }
 
-    menuToggle.addEventListener('click', function() {
-      menu.classList.toggle('show');
-    });
-  });
+  // Smooth scroll
+  const links = document.querySelectorAll('a[href^="#"]');
+  if (links.length > 0) {
+      for (const link of links) {
+          link.addEventListener('click', function (e) {
+              e.preventDefault();
+              const targetId = this.getAttribute('href').substring(1);
+              const targetElement = document.getElementById(targetId);
+
+              if (targetElement) {
+                  window.scrollTo({
+                      top: targetElement.offsetTop,
+                      behavior: 'smooth',
+                  });
+              }
+          });
+      }
+  }
+});
