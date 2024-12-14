9c630fb7f58cb8c4a2dfbe481a80f57ba40b944c
TiboTsr <tibo.tessier@gmail.com>
2024-12-14 11:52:44 +0100

    Boulangerie_1.3

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 // Menu déroulant
 document.addEventListener('DOMContentLoaded', function () {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('nav ul');
-
-  if (menuToggle && menu) {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('nav ul');
+  
+    if (menuToggle && menu) {
       // Gère le clic sur le bouton de menu
       menuToggle.addEventListener('click', function () {
-          menu.classList.toggle('show');
+        menu.classList.toggle('show');
       });
-  }
-
-  // Smooth scroll
-  const links = document.querySelectorAll('a[href^="#"]');
-  if (links.length > 0) {
+    }
+  
+    // Défilement fluide
+    const links = document.querySelectorAll('a[href^="#"]');
+    if (links.length > 0) {
       for (const link of links) {
-          link.addEventListener('click', function (e) {
-              e.preventDefault();
-              const targetId = this.getAttribute('href').substring(1);
-              const targetElement = document.getElementById(targetId);
-
-              if (targetElement) {
-                  window.scrollTo({
-                      top: targetElement.offsetTop,
-                      behavior: 'smooth',
-                  });
-              }
-          });
+        link.addEventListener('click', function (e) {
+          e.preventDefault();
+          const targetId = this.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetId);
+  
+          if (targetElement) {
+            window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: 'smooth',
+            });
+          }
+        });
       }
-  }
-});
+    }
+  });
